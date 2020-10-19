76ca5efea0271daa64afcc388e344aa2f6e74f0f
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-10-19 11:26:51 +0200

    feat: add styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import dayjs from 'dayjs';
-import Plotly from 'plotly.js-basic-dist';
-import React, { useCallback, useEffect, useState } from 'react';
-import createPlotlyComponent from 'react-plotly.js/factory';
-import Footer from './components/Footer';
-import OverlaySpinner from './components/OverlaySpinner';
-import PlayPause from './components/PlayPause';
-import Slider from './components/Slider';
-import { inputControlColors } from './helpers/colors';
-import getAngle from './helpers/getAngle';
-import getBaseLayout from './helpers/getBaseLayout';
-import getFrames from './helpers/getFrames';
-import getTraces from './helpers/getTraces';
-import useAutoCounter from './hooks/useAutoCounter';
-import useBodyClasses from './hooks/useBodyClasses';
-import useCovidData from './hooks/useCovidData';
+import dayjs from "dayjs";
+import Plotly from "plotly.js-basic-dist";
+import React, { useCallback, useEffect, useState } from "react";
+import createPlotlyComponent from "react-plotly.js/factory";
+import Footer from "./components/Footer";
+import OverlaySpinner from "./components/OverlaySpinner";
+import PlayPause from "./components/PlayPause";
+import Slider from "./components/Slider";
+import { inputControlColors } from "./helpers/colors";
+import getAngle from "./helpers/getAngle";
+import getBaseLayout from "./helpers/getBaseLayout";
+import getFrames from "./helpers/getFrames";
+import getTraces from "./helpers/getTraces";
+import useAutoCounter from "./hooks/useAutoCounter";
+import useBodyClasses from "./hooks/useBodyClasses";
+import useCovidData from "./hooks/useCovidData";
 
 const DELTA_DAYS = 7;
 
@@ -38,7 +38,12 @@ export default () => {
     play,
     pause,
   } = useAutoCounter();
-  const { byRegionAndDate, regions, dates, loading: loadingRawData } = useCovidData(DELTA_DAYS);
+  const {
+    byRegionAndDate,
+    regions,
+    dates,
+    loading: loadingRawData,
+  } = useCovidData(DELTA_DAYS);
   const [chartReady, setChartReady] = useState(false);
   const [pageReady, setPageReady] = useState(false);
 
@@ -66,7 +71,7 @@ export default () => {
       getFrames(getTraces(byRegionAndDate, dates), regions).map((f) => ({
         ...f,
         layout: { ...baseLayout, ...f.layout },
-      })),
+      }))
     );
   }, [
     byRegionAndDate,
@@ -113,10 +118,10 @@ export default () => {
         if (idx > regions.length) return;
 
         if (hoveredTraces.has(idx)) {
-          trace.line.color = '#e53e3e';
+          trace.line.color = "#e53e3e";
           trace.line.width = 2;
         } else {
-          trace.line.color = 'gray';
+          trace.line.color = "gray";
           trace.line.width = 0.5;
         }
       });
@@ -124,7 +129,7 @@ export default () => {
       setLayout(frames[day - 1].layout);
       setRevision((r) => r + 1);
     },
-    [frames, hoveredTraces, regions],
+    [frames, hoveredTraces, regions]
   );
 
   useEffect(() => {
@@ -134,10 +139,10 @@ export default () => {
         if (idx > regions.length) return;
 
         if (hoveredTraces.has(idx)) {
-          trace.line.color = '#e53e3e';
+          trace.line.color = "#e53e3e";
           trace.line.width = 2;
         } else {
-          trace.line.color = '#2d3748';
+          trace.line.color = "#2d3748";
           trace.line.width = 0.5;
         }
       });
@@ -155,126 +160,107 @@ export default () => {
   };
 
   const handleUnhover = (e) => {
-    const regionsIdx = new Set(e.points.reduce((r, p) => [...r, p.curveNumber], []));
+    const regionsIdx = new Set(
+      e.points.reduce((r, p) => [...r, p.curveNumber], [])
+    );
     setHoveredTraces((t) => new Set([...t].filter((i) => !regionsIdx.has(i))));
   };
 
   return (
-    <div
-      className="font-sans"
-      style={{
-        flexDirection: 'column',
-        flexWrap: 'nowrap',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100vw',
-        minHeight: '100vh',
-      }}
-    >
-      <div
-        className="font-sans"
+    <div className="font-sans flex flex-no-wrap flex-col justify-between items-center w-screen">
+      <header className="w-full flex flex-no-wrap flex-col justify-center items-center">
+        <div className="w-full bg-red-600 shadow-md ">
+          <p className="animate__animated animate__fadeIn font-bold text-3xl text-white w-full text-center my-4">
+            COVID-19 Growth in Italian Regions
+          </p>
+        </div>
+        <p className="mt-8 max-w-xl text-justify">
+          This interactive chart compares the number of new weekly cases with
+          the total number of confirmed cases. It is plotted on a logarithmic
+          scale so that exponential growth, i.e. cases doubling every week,
+          corresponds to a straight line.
+        </p>
+      </header>
+      <main
         style={{
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          display: 'flex',
-          justifyContent: 'start',
-          alignItems: 'center',
-          width: '100vw',
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          // flexGrow: 1,
+          width: "100%",
+          border: "3px solid green",
         }}
       >
-        <header
-          style={{
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
+        <OverlaySpinner
+          loading={!chartReady}
+          duration={1000}
+          onAnimationEnd={useCallback(() => setPageReady(true), [setPageReady])}
         >
-          <h1 className="font-bold text-3xl bg-red-600 text-white w-screen text-center p-4 shadow-md">
-            COVID-19 Growth in Italian Regions
-          </h1>
-          <p className="mt-4">Some text here</p>
-          <p>Some more text here</p>
-        </header>
-        <main
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            // flexGrow: 1,
-            width: '100%',
-          }}
-        >
-          <OverlaySpinner
-            loading={!chartReady}
-            duration={1000}
-            onAnimationEnd={useCallback(() => setPageReady(true), [setPageReady])}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              // flexGrow: 1,
+              width: "100%",
+              // height: '100%',
+            }}
           >
+            <Plot
+              data={traces}
+              layout={layout}
+              revision={revision}
+              useResizeHandler={true}
+              onInitialized={(_, graphDiv) => setPlotlyDiv(graphDiv)}
+              // onUpdate={setFigure}
+              onHover={handleHover}
+              onUnhover={handleUnhover}
+              onRelayout={adjustAnnotationAngle}
+              style={{ width: "100%" }}
+              config={plotlyConfig}
+            />
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-evenly',
-                alignItems: 'center',
-                // flexGrow: 1,
-                width: '100%',
-                // height: '100%',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "80%",
               }}
+              className="mt-4"
             >
-              <Plot
-                data={traces}
-                layout={layout}
-                revision={revision}
-                useResizeHandler={true}
-                onInitialized={(_, graphDiv) => setPlotlyDiv(graphDiv)}
-                // onUpdate={setFigure}
-                onHover={handleHover}
-                onUnhover={handleUnhover}
-                onRelayout={adjustAnnotationAngle}
-                style={{ width: '100%' }}
-                config={plotlyConfig}
-              />
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  width: '80%',
-                }}
-                className="mt-4"
+              <PlayPause onClick={playing ? pause : play} playing={playing} />
+              <span
+                style={{ minWidth: "15rem" }}
+                className="text-center text-2xl "
               >
-                <PlayPause onClick={playing ? pause : play} playing={playing} />
-                <span style={{ minWidth: '15rem' }} className="text-center text-2xl ">
-                  {dates && dayjs(dates[currentDay - 1]).format('MMMM D YYYY')}
-                </span>
-                <Slider
-                  step={1}
-                  min={0}
-                  max={numDays - 2}
-                  value={currentDay}
-                  onChange={setCurrentDay}
-                  styles={{
-                    track: {
-                      width: '100%',
-                    },
-                    active: {
-                      backgroundColor: inputControlColors,
-                    },
-                    thumb: {
-                      width: '1.5rem',
-                      height: '1.5rem',
-                      backgroundColor: inputControlColors,
-                    },
-                  }}
-                />
-              </div>
+                {dates && dayjs(dates[currentDay - 1]).format("MMMM D YYYY")}
+              </span>
+              <Slider
+                step={1}
+                min={0}
+                max={numDays - 2}
+                value={currentDay}
+                onChange={setCurrentDay}
+                styles={{
+                  track: {
+                    width: "100%",
+                  },
+                  active: {
+                    backgroundColor: inputControlColors,
+                  },
+                  thumb: {
+                    width: "1.5rem",
+                    height: "1.5rem",
+                    backgroundColor: inputControlColors,
+                  },
+                }}
+              />
             </div>
-          </OverlaySpinner>
-        </main>
-      </div>
+          </div>
+        </OverlaySpinner>
+      </main>
       <Footer />
     </div>
   );
--- src/helpers/getBaseLayout.js
+++ src/helpers/getBaseLayout.js
@@ -1,49 +1,49 @@
-import flatMap from 'lodash/fp/flatMap';
-import flow from 'lodash/fp/flow';
-import get from 'lodash/fp/get';
-import map from 'lodash/fp/map';
-import max from 'lodash/fp/max';
-import values from 'lodash/fp/values';
+import flatMap from "lodash/fp/flatMap";
+import flow from "lodash/fp/flow";
+import get from "lodash/fp/get";
+import map from "lodash/fp/map";
+import max from "lodash/fp/max";
+import values from "lodash/fp/values";
 
 const maxCases = (byRegionAndDate, label) =>
   flow(values, flatMap(values), map(get(label)), max)(byRegionAndDate);
 
 const getBaseLayout = (byRegionAndDate) => ({
   xaxis: {
-    type: 'log',
-    title: 'Total confirmed cases',
-    range: [0, Math.log10(maxCases(byRegionAndDate, 'totalCases') * 1.1)],
+    type: "log",
+    title: "Total cases",
+    range: [0, Math.log10(maxCases(byRegionAndDate, "totalCases") * 1.1)],
     fixedrange: true,
     showline: true,
-    linecolor: 'rgb(224,224,224)',
+    linecolor: "rgb(224,224,224)",
     titlefont: {
       size: 24,
-      color: '#1a202c',
+      color: "#1a202c",
     },
     automargin: true,
   },
   yaxis: {
-    type: 'log',
-    title: 'New confirmed cases<br>in the previous week',
+    type: "log",
+    title: "New cases in the previous week",
     titlefont: {
       size: 24,
-      color: '#1a202c',
+      color: "#1a202c",
     },
     range: [0, Math.log10(100000)],
     fixedrange: true,
     showline: true,
-    linecolor: 'rgb(224,224,224)',
+    linecolor: "rgb(224,224,224)",
     automargin: true,
   },
   responsive: true,
   autosize: true,
   showlegend: false,
-  paper_bgcolor: 'rgba(255,255,255,0)',
-  plot_bgcolor: 'rgba(255,255,255,0)',
-  hovermode: 'closest',
-  hoverlabel: { bgcolor: 'white', font: { size: 16, color: '#1a202c' } },
+  paper_bgcolor: "rgba(255,255,255,0)",
+  plot_bgcolor: "rgba(255,255,255,0)",
+  hovermode: "closest",
+  hoverlabel: { bgcolor: "white", font: { size: 16, color: "#1a202c" } },
   font: {
-    color: '#2d3748',
+    color: "#2d3748",
     family:
       'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
   },
