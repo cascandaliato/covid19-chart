a576c2acece331467ce404d0910b874e429dd2b8
casca <8927157+casca@users.noreply.github.com>
2020-11-15 15:48:37 +0100

    feat(accessibility): make play-pause button accessible

--- src/components/PlayPause.js
+++ src/components/PlayPause.js
@@ -1,11 +1,12 @@
-import classNames from 'classnames';
-import React from 'react';
-import styles from './PlayPause.module.css';
+import classNames from "classnames";
+import React from "react";
+import styles from "./PlayPause.module.css";
 
 const PlayPause = ({ playing, onClick }) => (
   <button
     onClick={onClick}
     className={classNames(styles.button, { [styles.paused]: playing })}
+    aria-label="Play/Pause"
   ></button>
 );
 
